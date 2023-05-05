3fb0eb5c0f7ec301ca690dd666e6164e14f6a91a
Xiao kai <2720640385@qq.com>
2023-05-05 18:12:04 +0800

    第二次提交,修改了App.java

--- src/main/java/org/example/git01/App.java
+++ src/main/java/org/example/git01/App.java
@@ -3,5 +3,6 @@ package org.example.git01;
 public class App {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git second commit");
     }
 }
