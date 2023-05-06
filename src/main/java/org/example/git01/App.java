0668130f489859b194e1b864adcaabce00395582
Xiaokai <90685423+2720640385@users.noreply.github.com>
2023-05-06 16:54:04 +0800

    Update App.java

--- src/main/java/org/example/git01/App.java
+++ src/main/java/org/example/git01/App.java
@@ -10,5 +10,6 @@ public class App {
         System.out.println("hello git test");
         System.out.println("hello git test");
         System.out.println("hello github !!! xiao kai master first push ");
+         System.out.println("hello github !!! xiao kai master first fetch ");
     }
 }
