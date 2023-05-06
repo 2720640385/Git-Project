8c55a8904da297b6a8473ecb74c9dba32adb1a62
Xiao kai <2720640385@qq.com>
2023-05-06 09:05:10 +0800

    hotfix branch first commit

--- src/main/java/org/example/git01/App.java
+++ src/main/java/org/example/git01/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git second commit");
+        System.out.println("hello git hotfix first commit");
     }
 }
