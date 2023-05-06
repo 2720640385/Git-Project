5d86b8969432cdf2b5a971ee72ccda815a42f630
Xiao kai <2720640385@qq.com>
2023-05-06 10:16:37 +0800

    master master fourth commit

--- src/main/java/org/example/git01/App.java
+++ src/main/java/org/example/git01/App.java
@@ -7,5 +7,6 @@ public class App {
         System.out.println("hello git hotfix first commit");
         System.out.println("hello git dev01 first commit");
         System.out.println("hello git test");
+        System.out.println("hello git test");
     }
 }
